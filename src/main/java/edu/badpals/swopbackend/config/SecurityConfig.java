f8e996bc933973952aee449f8dda49aef425299a
Sergio <serprigar@gmail.com>
2025-05-04 02:36:35 +0200

    feat(SecurityConfig.java): update API security to Users, non-Users and ADMINS

--- src/main/java/edu/badpals/swopbackend/config/SecurityConfig.java
+++ src/main/java/edu/badpals/swopbackend/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package edu.badpals.swopbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,10 +11,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**", "/v3/api-docs/**", // Swagger
+                                "/api/products/**", "/api/categories/**", "/api/public/**", // Tienda pública
+                                "/api/auth/**" // Login, registro, etc.
+                        ).permitAll()
+                        .anyRequest().authenticated() // lo demas con login
+                ).httpBasic(Customizer.withDefaults()); // Basic Auth;
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -21,8 +39,7 @@ public class SecurityConfig {
                 .password(passwordEncoder().encode("root"))
                 .roles("ADMIN")
                 .build();
-
-        return new InMemoryUserDetailsManager(user); // Usuario en memoria
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
@@ -30,4 +47,3 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
